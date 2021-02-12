419a2e90641371bdf18a01dda09e40d49b169484
DCTech <412200533@qq.com>
2021-02-13 01:52:11 +0800

    test pod

---
+++ NCNN_iOS_Swift/Classes/Header/NCNN_iOS_Swift-Swift.h
@@ -0,0 +1,12 @@
+//
+//  NCNN_iOS_Swift-Swift.h
+//  Pods
+//
+//  Created by 吕仕成 on 2021/2/13.
+//
+
+#ifndef NCNN_iOS_Swift_Swift_h
+#define NCNN_iOS_Swift_Swift_h
+
+
+#endif /* NCNN_iOS_Swift_Swift_h */
